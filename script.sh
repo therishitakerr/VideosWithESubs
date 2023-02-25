1542649bffa90e046903fd36bba72a8366564853
therishitakerr <75129590+therishitakerr@users.noreply.github.com>
2023-02-25 01:01:50 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 if [ ! -d "no_subtitles" ]; then
-    mkdir no_subtitles
+	mkdir no_subtitles
 fi
 
 for file in *; do
-    if [ -f "$file" ]; then
-        if [[ ($file == *.mkv) || ($file == *.mp4) || ($file == *.wmv) || ($file == *.mov) ]]; then
-            present=$(
-                ffmpeg -i "$file" -c copy -map 0:s:0 -frames:s 1 -f null - -v 0 -hide_banner
-                echo $?
-            )
-            echo $present
-            if [ "$present" == 1 ]; then
-                mv "$file" no_subtitles
-            fi
-        fi
-    fi
+	if [ -f "$file" ]; then
+		if [[ ($file == *.mkv) || ($file == *.mp4) || ($file == *.wmv) || ($file == *.mov) ]]; then
+			present=$(
+			mediainfo --Inform="General;%TextCount%" "$file"
+		)
+		echo $present
+		if [[ "$present" < 0 ]]; then
+			if [ ! -d "no_subtitles" ]; then
+				mkdir no_subtitles
+			fi
+			mv "$file" no_subtitles
+		fi
+		fi
+	fi
 done
-exit
